a3ba7d1766e952b5570ba6172e5552882626cc62
Toni Menzel <toni.menzel@rebaze.com>
2015-03-10 09:27:20 +0100

    Added test for deep union which currently fails. Next thing to work on.

--- src/test/java/org/auxis/commons/tree/operators/UnionTreeCombinerTest.java
+++ src/test/java/org/auxis/commons/tree/operators/UnionTreeCombinerTest.java
@@ -63,4 +63,23 @@ public class UnionTreeCombinerTest
         assertEquals( "d0941e", union.select( selector( "p3" ) ).fingerprint().substring( 0, 6 ) );
     }
 
+    @Test
+    public void testDeepUnion() {
+        TreeBuilder sn1 = session.createTreeBuilder();
+        sn1.branch(selector("p1")).branch( selector ("deep") ).add("one".getBytes());
+        sn1.branch(selector("p2")).add( "two".getBytes() );
+
+        TreeBuilder sn2 = session.createTreeBuilder();
+        sn2.branch(selector("p1")).add( "one".getBytes() );
+        sn2.branch(selector("p3")).add( "other".getBytes() );
+
+        TreeIndex union = wrapAsIndex( session.union( sn1.seal(), sn2.seal() ) );
+        formatter.prettyPrint( sn1.seal(), sn2.seal(), union );
+
+        assertEquals( 3, union.branches().length );
+        assertEquals( "fe05bc", union.select( selector( "p1" ) ).fingerprint().substring( 0, 6 ) ) ;
+        assertEquals( "ad782e", union.select( selector( "p2" ) ).fingerprint().substring( 0, 6 ) );
+        assertEquals( "d0941e", union.select( selector( "p3" ) ).fingerprint().substring( 0, 6 ) );
+    }
+
 }
