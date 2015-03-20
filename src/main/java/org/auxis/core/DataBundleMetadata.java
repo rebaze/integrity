3050de625cc2f408015657b4df72dd0a1684af43
Toni Menzel <toni.menzel@rebaze.com>
2015-03-20 18:31:55 +0100

    File headers

--- src/main/java/org/auxis/core/DataBundleMetadata.java
+++ src/main/java/org/auxis/core/DataBundleMetadata.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015 rebaze GmbH
+ * All rights reserved.
+ *
+ * This library and the accompanying materials are made available under the terms of the Apache License Version 2.0,
+ * which accompanies this distribution and is available at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ */
 package org.auxis.core;
 import java.net.URI;
 
